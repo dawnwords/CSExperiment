dce656a0be2a9585216e2bebde1b6b8dc00d1d05
Gu Jingxiao <gujingxiao@gmail.com>
2015-08-06 20:23:31 +0800

    extract Comparator for NaiveAlgorithms

--- src/main/java/cn/edu/fudan/se/crowdservice/algorithm/NaiveAlgorithm.java
+++ src/main/java/cn/edu/fudan/se/crowdservice/algorithm/NaiveAlgorithm.java
@@ -1,36 +1,54 @@
 package cn.edu.fudan.se.crowdservice.algorithm;
 
+import cn.edu.fudan.se.crowdservice.bean.AlgorithmParameter;
+import cn.edu.fudan.se.crowdservice.bean.TimeCost;
+import com.microsoft.schemas._2003._10.Serialization.Arrays.CSResultNum;
+import com.microsoft.schemas._2003._10.Serialization.Arrays.CSWorker;
+import sutd.edu.sg.CrowdWorker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
-import sutd.edu.sg.CrowdWorker;
-import cn.edu.fudan.se.crowdservice.bean.AlgorithmParameter;
-import cn.edu.fudan.se.crowdservice.bean.GlobalOptimizeResult;
-
 public abstract class NaiveAlgorithm implements Algorithm {
-	private int time;
-    private int cost;
-	@Override
-	public GlobalOptimizeResult globalOptimize(AlgorithmParameter parameter) {
-		// TODO Auto-generated method stub
-		List<CrowdWorker> cwList = rank(parameter);
-		long time = cwList.get(0).getResponseTime();
-		double cost = 0;
-		for(CrowdWorker cw : cwList) {
-			if(time < cw.getResponseTime()) time = cw.getResponseTime();
-			cost += cw.getCost();
-		}
-		GlobalOptimizeResult retVal = new GlobalOptimizeResult();
-		retVal.cost(cost);
-		retVal.time(time);
-		return retVal;
-	}
+    private Comparator<CrowdWorker> comparator;
 
-	@Override
-	public List<CrowdWorker> workerSelection(AlgorithmParameter parameter) {
-		// TODO Auto-generated method stub
-		return rank(parameter);
-	}
-	
-	abstract public List<CrowdWorker> rank(AlgorithmParameter parameter);
+    protected NaiveAlgorithm(Comparator<CrowdWorker> comparator) {
+        this.comparator = comparator;
+    }
 
+    @Override
+    public TimeCost globalOptimize(AlgorithmParameter parameter) {
+        List<CrowdWorker> cwList = rank(parameter);
+        long time = cwList.get(0).getResponseTime();
+        double cost = 0;
+        for (CrowdWorker cw : cwList) {
+            if (time < cw.getResponseTime()) time = cw.getResponseTime();
+            cost += cw.getCost();
+        }
+        TimeCost retVal = new TimeCost();
+        retVal.cost(cost);
+        retVal.time(time);
+        return retVal;
+    }
+
+    @Override
+    public List<CrowdWorker> workerSelection(AlgorithmParameter parameter) {
+        return rank(parameter);
+    }
+
+    public List<CrowdWorker> rank(AlgorithmParameter parameter) {
+        List<CSResultNum> retNums = parameter.resultNums();
+        List<CSWorker> csw = parameter.workers();
+        List<CrowdWorker> retCrowdWorker = new ArrayList<>();
+
+        for (int i = 0; i < retNums.size(); i++) {
+            CSWorker csWorker = csw.get(i);
+            CrowdWorker[] cw = csWorker.getValue();
+            Arrays.sort(cw, comparator);
+            retCrowdWorker.addAll(Arrays.asList(cw).subList(0, retNums.get(i).getValue()));
+        }
+        return retCrowdWorker;
+    }
 }
--- src/main/java/cn/edu/fudan/se/crowdservice/algorithm/OnlyReputationNavieAlgorithm.java
+++ src/main/java/cn/edu/fudan/se/crowdservice/algorithm/OnlyReputationNavieAlgorithm.java
@@ -1,41 +1,16 @@
 package cn.edu.fudan.se.crowdservice.algorithm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-import com.microsoft.schemas._2003._10.Serialization.Arrays.CSResultNum;
-import com.microsoft.schemas._2003._10.Serialization.Arrays.CSWorker;
-
 import sutd.edu.sg.CrowdWorker;
-import cn.edu.fudan.se.crowdservice.bean.AlgorithmParameter;
+
+import java.util.Comparator;
 
 public class OnlyReputationNavieAlgorithm extends NaiveAlgorithm {
 
-	@Override
-	public List<CrowdWorker> rank(AlgorithmParameter parameter) {
-		// TODO Auto-generated method stub
-		/*descending order.
-		 */
-		List<CSResultNum> retNums = parameter.resultNums();
-		List<CSWorker> csw = parameter.workers();
-		List<CrowdWorker> retCrowdWorker = new ArrayList<CrowdWorker>();
-
-		for(int i = 0; i < retNums.size() ; i++){
-			CSWorker csWorker = csw.get(i);
-			CrowdWorker[] cw = csWorker.getValue();
-			Arrays.sort(cw, new Comparator<CrowdWorker>(){ 
-	            public int compare(CrowdWorker arg0, CrowdWorker arg1) { 
-	                return -arg0.getReliability().compareTo(arg1.getReliability()); 
-	            } 
-	        });
-			
-			for(int j = 0; j < retNums.get(i).getValue() ; j++){
-				retCrowdWorker.add(cw[j]);
-			}
-		}
-		return retCrowdWorker;
-	}
+    protected OnlyReputationNavieAlgorithm() {
+        super(new Comparator<CrowdWorker>() {
+            public int compare(CrowdWorker arg0, CrowdWorker arg1) {
+                return -arg0.getReliability().compareTo(arg1.getReliability());
+            }
+        });
+    }
 }

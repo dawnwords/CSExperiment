ccc59c10df8256243b24fcafb1cd394178bd8ed7
Gu Jingxiao <gujingxiao@gmail.com>
2015-08-07 13:18:07 +0800

    + Main logic

--- src/main/java/cn/edu/fudan/se/crowdservice/Main.java
+++ src/main/java/cn/edu/fudan/se/crowdservice/Main.java
@@ -1,10 +1,27 @@
 package cn.edu.fudan.se.crowdservice;
 
+import cn.edu.fudan.se.crowdservice.algorithm.Algorithm;
+import cn.edu.fudan.se.crowdservice.algorithm.OnlyCostNaiveAlgorithm;
+import cn.edu.fudan.se.crowdservice.bean.ExpResult;
+import cn.edu.fudan.se.crowdservice.bean.ExperimentInput;
+import cn.edu.fudan.se.crowdservice.dao.GenerateInputDAO;
+import cn.edu.fudan.se.crowdservice.dao.SelectExperimentInputDAO;
+import cn.edu.fudan.se.crowdservice.explogic.Experiment;
+
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Dawnwords on 2015/8/6.
  */
 public class Main {
     public static void main(String[] args) {
-
+        Random random = new Random(807090336);
+        Algorithm algorithm = new OnlyCostNaiveAlgorithm();
+        new GenerateInputDAO().random(random).experimentTimes(3);
+        List<ExperimentInput> inputs = new SelectExperimentInputDAO().getResult();
+        for (ExperimentInput input : inputs) {
+            ExpResult expResult = new Experiment(input.random(random).algorithm(algorithm)).preform();
+        }
     }
 }

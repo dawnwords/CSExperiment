bc83bf0e006490f1592b70f7421e576267df5e06
Gu Jingxiao <gujingxiao@gmail.com>
2015-08-10 10:41:05 +0800

    + clear table option for ResetDBDAO

--- src/main/java/cn/edu/fudan/se/crowdservice/Main.java
+++ src/main/java/cn/edu/fudan/se/crowdservice/Main.java
@@ -2,10 +2,12 @@ package cn.edu.fudan.se.crowdservice;
 
 import cn.edu.fudan.se.crowdservice.algorithm.Algorithm;
 import cn.edu.fudan.se.crowdservice.algorithm.OnlyCostNaiveAlgorithm;
+import cn.edu.fudan.se.crowdservice.algorithm.TianHuatAlgorithm;
 import cn.edu.fudan.se.crowdservice.bean.ExpResult;
 import cn.edu.fudan.se.crowdservice.bean.ExperimentInput;
 import cn.edu.fudan.se.crowdservice.dao.GenerateInputDAO;
 import cn.edu.fudan.se.crowdservice.dao.GenerateWorkerDAO;
+import cn.edu.fudan.se.crowdservice.dao.ResetDBDAO;
 import cn.edu.fudan.se.crowdservice.dao.SelectExperimentInputDAO;
 import cn.edu.fudan.se.crowdservice.explogic.Experiment;
 import cn.edu.fudan.se.crowdservice.util.Logger;
@@ -17,26 +19,43 @@ import java.util.Random;
  * Created by Dawnwords on 2015/8/6.
  */
 public class Main {
-
     public static final Random RANDOM = new Random(807090336);
-    public static final Algorithm ALGORITHM = new OnlyCostNaiveAlgorithm();
+    public static final Algorithm ALGORITHM = new TianHuatAlgorithm();
+    public static final boolean CLEAR_EXP = true;
+    public static final boolean CLEAR_WORKER = false;
+
+    static {
+        System.setProperty("Debug", "true");
+    }
 
     public static void main(String[] args) {
+        resetDB();
         generateWorker(2000);
-        for (ExperimentInput input : generateExperimentInput(3, 400, 600)) {
+        for (ExperimentInput input : generateExperimentInput(1, 40, 60)) {
             ExpResult expResult = new Experiment(input.random(RANDOM).algorithm(ALGORITHM)).preform();
             Logger.info(expResult.toString());
         }
     }
 
+    private static void resetDB() {
+        ResetDBDAO reset = new ResetDBDAO();
+        if (!CLEAR_WORKER) {
+            reset.ignoreWorker();
+        }
+        if (!CLEAR_EXP) {
+            reset.ignoreExpinput().ignoreExpstatus();
+        }
+        reset.getResult();
+    }
+
     private static List<ExperimentInput> generateExperimentInput(int experimentInputNumber, int cs1GroupNumber, int cs2GroupNumber) {
-//        new GenerateInputDAO().random(RANDOM).experimentTimes(experimentInputNumber).getResult();
+        if (CLEAR_EXP) new GenerateInputDAO().random(RANDOM).experimentTimes(experimentInputNumber).getResult();
         Logger.info("Generate Experiment Input Finish: %d", experimentInputNumber);
         return new SelectExperimentInputDAO().cs1GroupNum(cs1GroupNumber).cs2GroupNum(cs2GroupNumber).getResult();
     }
 
     private static void generateWorker(int workerNumber) {
-//        new GenerateWorkerDAO().random(RANDOM).workerNumber(workerNumber).getResult();
+        if (CLEAR_WORKER) new GenerateWorkerDAO().random(RANDOM).workerNumber(workerNumber).getResult();
         Logger.info("Generate Worker Finish: %d", workerNumber);
     }
 }
--- src/main/java/cn/edu/fudan/se/crowdservice/dao/ResetDBDAO.java
+++ src/main/java/cn/edu/fudan/se/crowdservice/dao/ResetDBDAO.java
@@ -7,57 +7,83 @@ import java.sql.Statement;
  * Created by Dawnwords on 2015/8/6.
  */
 public class ResetDBDAO extends DAO<Boolean> {
+    private boolean ignoreExpinput, ignoreExpstatus, ignoreWorker;
+
     public static void main(String[] args) {
         new ResetDBDAO().getResult();
     }
 
+    public ResetDBDAO ignoreExpinput() {
+        this.ignoreExpinput = true;
+        return this;
+    }
+
+    public ResetDBDAO ignoreExpstatus() {
+        this.ignoreExpstatus = true;
+        return this;
+    }
+
+    public ResetDBDAO ignoreWorker() {
+        this.ignoreWorker = true;
+        return this;
+    }
+
     @Override
     protected Boolean processData(Connection connection) throws Exception {
+        if (ignoreExpinput && ignoreExpstatus && ignoreWorker) {
+            return true;
+        }
         Statement statement = connection.createStatement();
         statement.addBatch("SET FOREIGN_KEY_CHECKS=0");
-        statement.addBatch("DROP TABLE IF EXISTS `expinput`");
-        statement.addBatch("CREATE TABLE `expinput` (\n" +
-                "  `id` INT(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,\n" +
-                "  `cost` DOUBLE(20,2) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `deadline` INT(20) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs1resultNum` INT(10) DEFAULT NULL,\n" +
-                "  `cs1deadline` INT(20) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs1cost` DOUBLE(20,2) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs1realtime` INT(20) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs1realcost` DOUBLE(20,2) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs1realResultNum` INT(10) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs1success` BIT(1) DEFAULT NULL,\n" +
-                "  `cs2resultNum` INT(10) DEFAULT NULL,\n" +
-                "  `cs2deadline` INT(20) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs2cost` DOUBLE(20,2) DEFAULT NULL,\n" +
-                "  `cs2realtime` INT(20) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs2realcost` DOUBLE(20,2) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs2realResultNum` INT(10) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs2success` BIT(1) DEFAULT NULL,\n" +
-                "  PRIMARY KEY (`id`)\n" +
-                ") ENGINE=InnoDB AUTO_INCREMENT=0 CHARSET=utf8");
-        statement.addBatch("DROP TABLE IF EXISTS `expstatus`");
-        statement.addBatch("CREATE TABLE `expstatus` (\n" +
-                "  `id` INT(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,\n" +
-                "  `expid` INT(10) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `workerid` INT(10) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `cs` ENUM('CS2','CS1') DEFAULT NULL,\n" +
-                "  `selected` BIT(1) DEFAULT NULL,\n" +
-                "  `success` BIT(1) DEFAULT NULL,\n" +
-                "  PRIMARY KEY (`id`),\n" +
-                "  KEY `FK_expid` (`expid`),\n" +
-                "  KEY `FK_workerid` (`workerid`),\n" +
-                "  CONSTRAINT `FK_expid` FOREIGN KEY (`expid`) REFERENCES `expinput` (`id`),\n" +
-                "  CONSTRAINT `FK_workerid` FOREIGN KEY (`workerid`) REFERENCES `worker` (`id`)\n" +
-                ") ENGINE=InnoDB AUTO_INCREMENT=0 CHARSET=utf8");
-        statement.addBatch("DROP TABLE IF EXISTS `worker`");
-        statement.addBatch("CREATE TABLE `worker` (\n" +
-                "  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,\n" +
-                "  `cost` DOUBLE(20,2) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `reliability` DOUBLE(20,18) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  `responseTime` INT(10) UNSIGNED ZEROFILL DEFAULT NULL,\n" +
-                "  PRIMARY KEY (`id`)\n" +
-                ") ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8");
+        if (!ignoreExpinput) {
+            statement.addBatch("DROP TABLE IF EXISTS `expinput`");
+            statement.addBatch("CREATE TABLE `expinput` (\n" +
+                    "  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,\n" +
+                    "  `cost` DOUBLE(20,2) UNSIGNED DEFAULT NULL,\n" +
+                    "  `deadline` INT(20) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs1resultNum` INT(10) DEFAULT NULL,\n" +
+                    "  `cs1deadline` INT(20) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs1cost` DOUBLE(20,2) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs1realtime` INT(20) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs1realcost` DOUBLE(20,2) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs1realResultNum` INT(10) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs1success` BIT(1) DEFAULT NULL,\n" +
+                    "  `cs2resultNum` INT(10) DEFAULT NULL,\n" +
+                    "  `cs2deadline` INT(20) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs2cost` DOUBLE(20,2) DEFAULT NULL,\n" +
+                    "  `cs2realtime` INT(20) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs2realcost` DOUBLE(20,2) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs2realResultNum` INT(10) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs2success` BIT(1) DEFAULT NULL,\n" +
+                    "  PRIMARY KEY (`id`)\n" +
+                    ") ENGINE=InnoDB AUTO_INCREMENT=0 CHARSET=utf8");
+        }
+        if (!ignoreExpstatus) {
+            statement.addBatch("DROP TABLE IF EXISTS `expstatus`");
+            statement.addBatch("CREATE TABLE `expstatus` (\n" +
+                    "  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,\n" +
+                    "  `expid` INT(10) UNSIGNED DEFAULT NULL,\n" +
+                    "  `workerid` INT(10) UNSIGNED DEFAULT NULL,\n" +
+                    "  `cs` ENUM('CS2','CS1') DEFAULT NULL,\n" +
+                    "  `selected` BIT(1) DEFAULT NULL,\n" +
+                    "  `success` BIT(1) DEFAULT NULL,\n" +
+                    "  PRIMARY KEY (`id`),\n" +
+                    "  KEY `FK_expid` (`expid`),\n" +
+                    "  KEY `FK_workerid` (`workerid`),\n" +
+                    "  CONSTRAINT `FK_expid` FOREIGN KEY (`expid`) REFERENCES `expinput` (`id`),\n" +
+                    "  CONSTRAINT `FK_workerid` FOREIGN KEY (`workerid`) REFERENCES `worker` (`id`)\n" +
+                    ") ENGINE=InnoDB AUTO_INCREMENT=0 CHARSET=utf8");
+        }
+        if (!ignoreWorker) {
+            statement.addBatch("DROP TABLE IF EXISTS `worker`");
+            statement.addBatch("CREATE TABLE `worker` (\n" +
+                    "  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,\n" +
+                    "  `cost` DOUBLE(20,2) UNSIGNED DEFAULT NULL,\n" +
+                    "  `reliability` DOUBLE(20,18) UNSIGNED DEFAULT NULL,\n" +
+                    "  `responseTime` INT(10) UNSIGNED DEFAULT NULL,\n" +
+                    "  PRIMARY KEY (`id`)\n" +
+                    ") ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8");
+        }
         return statement.executeBatch().length > 0;
     }
 }

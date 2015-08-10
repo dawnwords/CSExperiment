76b34b77f19861c990262fea4f7a9c5eb2b5a16b
Gu Jingxiao <gujingxiao@gmail.com>
2015-08-10 10:39:36 +0800

    change NaiveAlgorithm logic

--- src/main/java/cn/edu/fudan/se/crowdservice/algorithm/NaiveAlgorithm.java
+++ src/main/java/cn/edu/fudan/se/crowdservice/algorithm/NaiveAlgorithm.java
@@ -8,7 +8,7 @@ import com.microsoft.schemas._2003._10.Serialization.Arrays.CSWorker;
 import sutd.edu.sg.CrowdWorker;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,31 +21,42 @@ public abstract class NaiveAlgorithm implements Algorithm {
 
     @Override
     public TimeCost globalOptimize(AlgorithmParameter parameter) {
-        List<CSWorker> csWorkers = serviceTopK(parameter);
+        List<CSWorker> csWorkers = rank(parameter);
         TimeCost[] timeCosts = new TimeCost[csWorkers.size()];
 
         long timeTotal = 0;
         double costTotal = 0;
         for (int i = 0; i < timeCosts.length; i++) {
             timeCosts[i] = new TimeCost();
-            for (CrowdWorker cw : csWorkers.get(i).getValue()) {
-                timeCosts[i].aggregate(cw.getResponseTime(), cw.getCost());
+            CrowdWorker[] cw = csWorkers.get(i).getValue();
+            int resultNum = parameter.resultNumArray()[i].getValue();
+            for (int j = 0; j < cw.length && j < resultNum; j++) {
+                timeCosts[i].aggregate(cw[j].getResponseTime(), cw[j].getCost());
             }
             Logger.info(parameter.resultNumArray()[i].getKey() + " : " + timeCosts[i]);
             timeTotal += timeCosts[i].time();
             costTotal += timeCosts[i].cost();
         }
         return new TimeCost()
-                .time(parameter.deadline() * timeCosts[0].time() / timeTotal)
-                .cost(parameter.cost() * timeCosts[0].cost() / costTotal);
+                .time(timeTotal == 0 ? 0 : parameter.deadline() * timeCosts[0].time() / timeTotal)
+                .cost(costTotal == 0 ? 0 : parameter.cost() * timeCosts[0].cost() / costTotal);
     }
 
     @Override
     public List<CrowdWorker> workerSelection(AlgorithmParameter parameter) {
-        return Arrays.asList(serviceTopK(parameter).get(0).getValue());
+        CrowdWorker[] cw = rank(parameter).get(0).getValue();
+        double totalCost = parameter.cost();
+        ArrayList<CrowdWorker> result = new ArrayList<>();
+        for (CrowdWorker worker : cw) {
+            if (totalCost > worker.getCost()) {
+                totalCost -= worker.getCost();
+                result.add(worker);
+            }
+        }
+        return result;
     }
 
-    public List<CSWorker> serviceTopK(AlgorithmParameter parameter) {
+    public List<CSWorker> rank(AlgorithmParameter parameter) {
         List<CSResultNum> csResultNums = parameter.resultNums();
         List<CSWorker> csw = parameter.workers();
         List<CSWorker> result = new ArrayList<>();
@@ -53,10 +64,16 @@ public abstract class NaiveAlgorithm implements Algorithm {
         for (int i = 0; i < csResultNums.size(); i++) {
             CSWorker csWorker = csw.get(i);
             CrowdWorker[] cw = csWorker.getValue();
-            CrowdWorker[] newcs = Arrays.copyOf(cw, cw.length);
-            Arrays.sort(newcs, comparator);
 
-            result.add(new CSWorker(csWorker.getKey(), Arrays.copyOf(newcs, csResultNums.get(i).getValue())));
+            List<CrowdWorker> newcw = new ArrayList<>();
+            for (CrowdWorker w : cw) {
+                if (w.getResponseTime() < parameter.deadline()) {
+                    newcw.add(w);
+                }
+            }
+
+            Collections.sort(newcw, comparator);
+            result.add(new CSWorker(csWorker.getKey(), newcw.toArray(new CrowdWorker[newcw.size()])));
         }
         return result;
     }
